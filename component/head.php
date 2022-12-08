a0696171bd9ce2d6b32883f8a5da053a336ceee5
khen <adongokhen1@gmail.com>
2022-12-08 09:09:51 -0800

    first commit

---
+++ component/head.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bugLess</title>
+    <script src="https://kit.fontawesome.com/1cb0bb75dc.js" crossorigin="anonymous"></script>
+    <!-- Bootstrap5 CSS -->
+   <!--  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">-->
+ <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../index.css">
+    <link rel="stylesheet" href="nav.css">
+    <link rel="stylesheet" href="fonts-6/css/all.css">
+</head>
+</html>
---
+++ component/nav.php
@@ -0,0 +1,31 @@
+<nav class="navbar bar1 navbar-light">
+  <div class="container-fluid">
+    <ul class="list ml-auto">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About Us</a></li>
+    </ul>
+    <div class="logo">
+    <a href="#"><img src="img/logox.png"></a>
+    </div>
+    <div class="right">
+    <form class="d-flex mr-auto">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+    <div class="user mr-auto"><a href="#"><i class="fa-regular fa-user"></i></a></div>
+    </div>
+  </div>
+</nav>
+
+<nav class="navbar bar2">
+<div class="list2">
+  <ul>
+    <li><a href="#">Questions & Answers</a></li>
+    <li><a href="#">iCom</a></li>
+    <li><a href="#">1-ied Project</a></li>
+    <li><a href="#">Freelance</a></li>
+    <li><a href="#">Local Mentors</a></li>
+    <li><a href="#">Tutorials</a></li>
+  </ul>
+</div>
+</nav>
